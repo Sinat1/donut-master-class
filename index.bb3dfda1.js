017bd1dc844159ce09a423d61269dd2dc3d95e13
Sinat1 <67416969+Sinat1@users.noreply.github.com>
2022-06-01 06:40:56 +0000

    Deploying to gh-pages from @ Sinat1/donut-master-class@fc3ba76e2f2d13a9b30890cac92e42f16848ae34 🚀

---
+++ index.bb3dfda1.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{slidesPerView:1,centeredSlides:!0,spaceBetween:32,loop:!0,loopFillGroupWithBlank:!0,pagination:{el:".swiper-pagination",type:"fraction",clickable:!0},breakpoints:{320:{slidesPerView:1},768:{slidesPerView:3}}});
+//# sourceMappingURL=index.bb3dfda1.js.map
