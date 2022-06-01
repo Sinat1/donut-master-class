1d9921f578e59d31e18a3048a775600b4cba34e3
Sinat1 <67416969+Sinat1@users.noreply.github.com>
2022-06-01 12:55:15 +0000

    Deploying to gh-pages from @ Sinat1/donut-master-class@79cb879f7d9cef7268f1a8107e2e954a4c9afaa1 🚀

---
+++ index.77622b47.js
@@ -0,0 +1,2 @@
+var e;e=document.querySelector(".scroll-up"),window.addEventListener("scroll",(function(){var t=window.pageYOffset,o=document.documentElement.clientHeight;t>.5*o&&e.classList.add("scroll-up--show"),t<.5*o&&e.classList.remove("scroll-up--show")})),e.addEventListener("click",backToTop),e.addEventListener("touchstart",backToTop);
+//# sourceMappingURL=index.77622b47.js.map
